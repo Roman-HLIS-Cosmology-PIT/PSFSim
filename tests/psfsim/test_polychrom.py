567138608ca67966ff6ce3b73640094bb2f785a9
hirata10 <hirata.10@osu.edu>
2026-04-22 20:18:40 -0400

    new test in test_polychrom.py

--- tests/psfsim/test_polychrom.py
+++ tests/psfsim/test_polychrom.py
@@ -8,6 +8,8 @@ import numpy as np
 import pytest
 
 
+# --- These are tests for general functionality ---
+
 class _FakeBandpass:
     def __init__(self, blue_limit_nm, red_limit_nm, response=1.0):
         self.blue_limit = blue_limit_nm
@@ -141,3 +143,48 @@ def test_wavelengths_are_sorted_internally(patch_poly_deps):
     out_sorted = p_sorted.compute_poly_psf(postage_stamp_size=1, ovsamp=2, use_filter="H")
 
     assert np.allclose(out_unsorted, out_sorted)
+
+
+# -- This is a test for whether the output is reasonable --
+# (not using the above fixtures)
+
+def test_poly_h():
+    """Simple H-band test."""
+
+    # This will go out of the bandpass, and since req_in_band is True
+    # by default the final wavelengths don't get used.
+    p=psfsim.polychrom.PolychromaticPSF(6, 12.1, -2.2, np.linspace(1.4, 1.9, 6))
+    arr = p.compute_poly_psf(use_filter="H", ovsamp=8)
+
+    # These are to alert us to things that change.
+    # If you do a big enough model update, they might fail,
+    # and you should assess whether the change is reasonable.
+    assert 0.003 <= np.amax(arr) / np.sum(arr) <= 0.004
+
+    # check that the diffraction spikes are there
+    # draw a circle around the PSF and measure the Fourier modes in longitude
+    N = 64
+    sc = np.zeros(N)
+    for i in range(N):
+        x = 123.5 + 100.0 * np.cos(i * np.pi * 2.0 / N)
+        y = 123.5 + 100.0 * np.sin(i * np.pi * 2.0 / N)
+        xi = int(np.floor(x))
+        yi = int(np.floor(y))
+        xf = x - xi
+        yf = y - yi
+        sc[i] = arr[yi, xi] * (1 - xf) * (1 - yf) + arr[yi + 1, xi] * (1 - xf) * yf
+        sc[i] += arr[yi, xi + 1] * xf * (1 - yf) + arr[yi + 1, xi + 1] * xf * yf
+    sc /= np.mean(sc)
+    scft = np.fft.ifft(sc)
+    # for i in range(N//2+1): print(f"{scft[i].real:8.5f} {scft[i].imag:8.5f} {np.abs(scft[i]):8.5f}")
+
+    # expect lots of m=6 and m=12, less of the others
+    assert np.abs(scft[4]) < 0.075
+    assert np.abs(scft[5]) < 0.075
+    assert 0.1 < np.abs(scft[6]) < 0.2
+    assert np.abs(scft[7]) < 0.075
+    assert np.abs(scft[8]) < 0.075
+    assert np.abs(scft[11]) < 0.075
+    assert 0.1 < np.abs(scft[12]) < 0.2
+    assert np.abs(scft[13]) < 0.075
+
