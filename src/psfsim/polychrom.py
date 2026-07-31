d3879c6208a65f8ce8ff3f356b1ab0a3501df39d
Elise Moore <moore5248@cardinal-rw01.ten.osc.edu>
2026-07-31 12:50:35 -0400

    Added ghost parameter and attribute to PolychromaticPSF

--- src/psfsim/polychrom.py
+++ src/psfsim/polychrom.py
@@ -72,16 +72,20 @@ class PolychromaticPSF:
         Spectral energy distribution weight function evaluated as ``sed(wav_microns)``.
         This should be in units proportional to photons/m^2/s/micron.
         If ``None``, a flat spectral weight (in lambda F_lambda) is assumed.
+    ghost : bool, optional
+        Whether to include the ghost path from refraction in the optical filter.
+        Default: False. Only can be true if ray_trace=True.
 
     """
 
-    def __init__(self, scanum, scax, scay, wavelengths, sed=None):
+    def __init__(self, scanum, scax, scay, wavelengths, sed=None, ghost=False):
         self.scanum = scanum
         self.scax = scax
         self.scay = scay
         self.wavelengths = wavelengths  # replace with something better later
         self.sed = sed
         self.bandpass = galsim.roman.getBandpasses()
+        self.ghost = ghost
 
     def compute_poly_psf(
         self,
@@ -177,6 +181,9 @@ class PolychromaticPSF:
                 f"Provided range: [{wavelengths.min():.6g}, {wavelengths.max():.6g}] microns."
             )
 
+        if (not ray_trace) and (self.ghost):
+            raise ValueError("ghost must be False")
+
         def _compute_mono_image(wav):
             this_psf = PSFObject(
                 self.scanum,
@@ -192,6 +199,7 @@ class PolychromaticPSF:
                 extra_aberrations=extra_aberrations,
                 cycle=cycle,
                 mjd=mjd,
+                ghost=self.ghost,
                 perturbations=perturbations,
             )
             this_psf.get_optical_psf()
