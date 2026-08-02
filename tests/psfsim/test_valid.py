7ba836a62d1b8d872921f36b7f0ed29847e93f72
Christopher Hirata <christopherhirata@Christophers-MacBook-Air.local>
2026-08-02 19:49:23 -0400

    removed some deprecated functions

---
+++ tests/psfsim/test_valid.py
@@ -0,0 +1,22 @@
+"""Ensure that the correct exceptions are raised for invalid values."""
+
+import numpy as np
+import pytest
+from psfsim.opticspsf import GeometricOptics
+from psfsim.wfi_coordinate_transformations import from_sca_to_fpa, from_sca_to_analysis
+
+
+def test_scaerr():
+    """Invalid SCA numbers."""
+
+    for f in [from_sca_to_fpa, from_sca_to_analysis]:
+        for sca in [0, 19]:
+            with pytest.raises(ValueError):
+                f(sca, 0.0, 0.0)
+
+
+def test_cycleerr():
+    """Invalid cycle number."""
+
+    with pytest.raises(ValueError):
+        GeometricOptics(4, 1.5, -4.0, cycle=1).path_diff()
