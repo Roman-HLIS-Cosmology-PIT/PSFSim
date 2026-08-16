e897ab9ef7dc4290f2f47147fbef33f07b0a65c7
Christopher Hirata <christopherhirata@Christophers-MacBook-Air.local>
2026-08-15 21:42:38 -0400

    more tests of illumination at the boundaries

--- tests/psfsim/test_reflect.py
+++ tests/psfsim/test_reflect.py
@@ -2,6 +2,7 @@
 
 import galsim
 import numpy as np
+from psfsim.mtf_diffusion import intensity_to_image
 from psfsim.polychrom import PolychromaticPSF
 
 
@@ -96,3 +97,62 @@ def test_neumann():
     #
     # shouldn't be anything the other way
     assert np.all(np.abs(psfs[3][152:172, 252:272]) < 1.0e-6)
+
+
+def test_clip():
+    """Tests for clipping in intensity_to_image."""
+
+    # make a simple Gaussian image
+    sig = 4.0
+    sig2 = np.hypot(sig, 3.2)
+    _s = np.linspace(-20, 20, 21)
+    _x, _y = np.meshgrid(_s, _s)
+    intensity = np.exp(-0.5 * (_x**2 + _y**2) / sig**2)
+    n = 30
+    outmap = np.zeros((9, n, n))
+    _so = np.linspace(-(n - 1) / 2, (n - 1) / 2, n) * (_s[1] - _s[0])
+    _xo, _yo = np.meshgrid(_so, _so)
+
+    for j in range(9):
+        x_in = 20430.0 * (j % 3 - 1)
+        y_in = 20430.0 * (j // 3 - 1)
+        outmap[j, :, :] = intensity_to_image(
+            intensity, x_in, y_in, x_in, y_in, n, _s[1] - _s[0], reflect=True, tophat=False
+        )
+
+        im_target = np.zeros((n, n))
+
+        factor = sig**2 / sig2**2 / (_s[1] - _s[0]) ** 2
+        wx = np.exp(-0.5 * _xo**2 / sig2**2)
+        if j % 3 == 0:
+            wx += np.exp(-0.5 * (_xo + 20) ** 2 / sig2**2)
+        if j % 3 == 2:
+            wx += np.exp(-0.5 * (_xo - 20) ** 2 / sig2**2)
+        wy = np.exp(-0.5 * _yo**2 / sig2**2)
+        if j // 3 == 0:
+            wy += np.exp(-0.5 * (_yo + 20) ** 2 / sig2**2)
+        if j // 3 == 2:
+            wy += np.exp(-0.5 * (_yo - 20) ** 2 / sig2**2)
+        im_target = factor * wx * wy
+        assert np.sum((outmap[j] - im_target) ** 2) / np.sum(im_target**2) < 0.01
+
+    for j in range(9):
+        x_in = 20450.0 * (j % 3 - 1)
+        y_in = 20450.0 * (j // 3 - 1)
+        outmap[j, :, :] = intensity_to_image(
+            intensity, x_in, y_in, x_in, y_in, n, _s[1] - _s[0], reflect=True, tophat=False
+        )
+
+    sum = np.sum(outmap, axis=(1, 2))
+    ctr_x = np.sum(outmap * _xo[None, ...], axis=(1, 2)) / sum
+    ctr_y = np.sum(outmap * _yo[None, ...], axis=(1, 2)) / sum
+    print(sum)
+    print(ctr_x)
+    print(ctr_y)
+
+    sa = 1.27046717e-03
+    sb = 8.93446439e-02
+    sc = 6.28309457e00
+    assert np.all(np.log(sum / np.array([sa, sb, sa, sb, sc, sb, sa, sb, sa])) < 0.05)
+    assert np.all(np.abs(ctr_x - np.array([10, 0, -10, 10, 0, -10, 10, 0, -10])) < 0.01)
+    assert np.all(np.abs(ctr_y - np.array([10, 10, 10, 0, 0, 0, -10, -10, -10])) < 0.01)
